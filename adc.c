17a9eb3350441c09b394139b08bf6abbea054ece
David Winant <davewinant@gmail.com>
2020-12-31 13:13:30 -0500

    modified UART transmit to use interrupt-driven response

--- adc.c
+++ adc.c
@@ -16,22 +16,25 @@ extern HAL_StatusTypeDef HAL_ADC_Start_DB_DMA (ADC_HandleTypeDef* hadc, uint32_t
 
 uint16_t mem[2][BUF0_SIZE] = {{0}, {0}};
 uint32_t conversion_count[2] = {0};
+uint32_t processed_count[2] = {0};
 
 int buffer_to_process = 0;
 int go_ahead_process_buffer = FALSE;
 
 
-static inline flag_up (void)
+__attribute__((unused))
+static inline void flag_up (void)
 {
 	HAL_GPIO_WritePin (ARDUINO_D2_GPIO_Port, ARDUINO_D2_Pin, 1);
 }
 
-static inline flag_down (void)
+static inline void flag_down (void)
 {
 	HAL_GPIO_WritePin (ARDUINO_D2_GPIO_Port, ARDUINO_D2_Pin, 0);
 }
 
-static inline flag_set (int v)
+
+static inline void flag_set (int v)
 {
 	HAL_GPIO_WritePin (ARDUINO_D2_GPIO_Port, ARDUINO_D2_Pin, v);
 }
@@ -43,13 +46,19 @@ void process_buffer (int which)
 	static int processed = 0;
 	uint32_t sum = 0;
 
+	processed_count[which]++;
 	for (int i = 0; i < BUF0_SIZE; i++)
 		sum += mem[which][i];
 
 	if (++processed % 377 == 0) {
 		flag_up();
-		output ("%c %x\r\n", '0'+which, sum / BUF0_SIZE);
+		output ("  %c %x\r\n", '0'+which, sum / BUF0_SIZE);
 		flag_down();
+	}
+
+	// simulate one in 10 longer processing delay
+	if (processed % 1000000 == 0) {
+		HAL_Delay(25);
 	}
 }
 
@@ -82,6 +91,7 @@ void adc_process (void)
   output ("A/D started\r\n");
 
   uint32_t last_report = HAL_GetTick();
+  uint32_t last_conversions = 0;
   while (1) {
 	  if (go_ahead_process_buffer) {
 		  int which = buffer_to_process;
@@ -89,8 +99,17 @@ void adc_process (void)
 
 		  process_buffer (which);
 	  } else if (HAL_GetTick() - last_report > 5000) {
-		  output ("c %6d %6d\r\n", conversion_count[0], conversion_count[1]);
-		  last_report = HAL_GetTick();
+		  uint32_t conversions = conversion_count[0] + conversion_count[1];
+		  uint32_t report_time = HAL_GetTick();
+
+		  if (conversions > last_conversions) {
+			  int period_us = (report_time - last_report) * 1000 / (conversions - last_conversions);
+              output ("c %6d/%d %6d/%d us/buffer %d\r\n", processed_count[0], conversion_count[0], processed_count[1], conversion_count[1], period_us);
+		  } else {
+			  output ("c %6d/%d %6d/%d\r\n", processed_count[0], conversion_count[0], processed_count[1], conversion_count[1]);
+		  }
+		  last_report = report_time;
+		  last_conversions = conversions;
 	  }
 
   }
--- output.c
+++ output.c
@@ -11,12 +11,38 @@
 #include <stdio.h>
 
 extern UART_HandleTypeDef huart1;
+static UART_HandleTypeDef *huart = &huart1;
 
+static volatile int huart_inprogress = 0;
+
+/**
+  * @brief Tx Transfer completed callback.
+  * @param huart UART handle.
+  * @retval None
+  */
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if (huart_inprogress)
+		huart_inprogress = 0;
+}
+
+static void wait_until_buffer_ready (void)
+{
+	while (huart_inprogress) HAL_Delay(1);
+}
 
 void output_string (const char* s)
 {
-	int l = strlen(s);
-	HAL_UART_Transmit (&huart1, (uint8_t*) s, l, 1000);
+	int len = strlen(s);
+
+	while (huart_inprogress) HAL_Delay(1);
+	HAL_Delay(5);
+
+	huart_inprogress = 1;
+
+	if (HAL_UART_Transmit_IT (huart, (uint8_t*) s, len) != HAL_OK) {
+		huart_inprogress = 0;
+	}
 }
 
 
@@ -24,6 +50,8 @@ void output (const char* format, ...)
 {
 	static char outline[200];
 	va_list		va;
+
+	wait_until_buffer_ready();
 
 	va_start (va, format);
 	vsnprintf (outline, sizeof(outline), format, va);
